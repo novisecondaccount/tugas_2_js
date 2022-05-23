94b64b8ea4b24feaaf9b3ed2f94562cfff77fd06
novifitri <animangap@gmail.com>
2022-05-23 20:07:17 +0700

    init

---
+++ tugas2.js
@@ -0,0 +1,13 @@
+var number = 1
+var str = "Saya suka coding"
+var bool = true
+var nul = null
+var kosong
+var prima = [2,3,7,11]
+
+console.log("Angka :" +number)
+console.log("String :"+str)
+console.log("Boolean :" +bool)
+console.log("Null : " +nul)
+console.log("Undefined :" +kosong)
+console.log("Bil prima : " +prima)
